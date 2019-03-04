8e741356d67fcb41a3dc6edd28c7489b0358c29b
taobear <1743909352@qq.com>
2019-03-05 00:53:09 +0800

    construct init

--- allocator/tb_construct.h
+++ allocator/tb_construct.h
@@ -1,7 +1,42 @@
 #ifndef __TB_CONSTRUCT_H_
 #define __TB_CONSTRUCT_H_
 
+#include "tb_type_traits.h"
+#include "iterator/tb_iterator.h"
+
 namespace tbSTL {
+
+template <class T1, class T2>
+inline void construct(T1 *pointer, const T2& val) {
+    new (pointer) T1(val);
+}
+template <class T>
+inline void destroy(T *pointer) {
+    pointer->~T();
+}
+
+template <class ForwardIter>
+inline void __destroy_aux(ForwardIter first, ForwardIter last, true_type) {
+
+}
+template <class ForwardIter>
+inline void __destroy_aux(ForwardIter first, ForwardIter last, false_type) {
+    for ( ; first != last; ++first) {
+        destroy(&*first);
+    }
+}
+
+template <class ForwardIter, class T>
+inline void __destroy(ForwardIter first, ForwardIter last, T*) {
+    __destroy_aux(first, last, is_trivally_destructible<T>());
+}
+
+template <class ForwardIter>
+inline void destroy(ForwardIter first, ForwardIter last) {
+    __destroy(first, last, value_type(first));
+}
+template <> inline void destroy(char*, char*) {}
+template <> inline void destroy(wchar_t*, wchar_t*) {}
 
 } // namespace tbSTL
 
